51333d145de0069332eaf7739cdb49d6199533df
sidney2k1 <sidsingla02@gmail.com>
2024-12-10 17:10:10 +0530

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+#import necessary libraries
+from tkinter import *
+#create window
+window=Tk()
+#set the window titles and geometry
+window.title("Demo Window")
+window.geometry("400x300")
+window.mainloop()
